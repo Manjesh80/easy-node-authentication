c38f2b19723a21f40c0cdacbf5026854f84a1307
Manjesh Gowda <manjesh.2j@gmail>
2014-09-17 18:49:47 -0400

    DB Connection string update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://dbadmin:test@localhost:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://<user>:<pass>@localhost:27017/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
